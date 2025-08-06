f758526b6ccf7f9827463c183f77d76fbf69d70e
Benjamin <105524918+Benjamin-Hogan@users.noreply.github.com>
2025-08-05 20:17:13 -0700

    Simplify LVGL setup to build without hardware driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <lvgl.h>
-#include <lvgl_helpers.h>
+// The `lvgl_helpers` utilities from the ESP32 specific driver package
+// require hardware configuration that isn't provided in this project. To
+// keep the example buildable on any system, we only depend on the core LVGL
+// library and avoid hardware initialisation here.
 
 // Simple LVGL application displaying a label
 
 void setup() {
     lv_init();
-    lvgl_driver_init();
 
     lv_obj_t * label = lv_label_create(lv_scr_act());
     lv_label_set_text(label, "Hello, LVGL!");
